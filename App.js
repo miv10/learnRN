59e8b3175a12cac8ee2cf8969956344f474ede79
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2023-10-16 08:09:06 +0100

    wip

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  function goalInputHandler(enteredText) {
+    console.log(enteredText);
+  }
+
+  function addGoalHandler() {}
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal" />
-        <Button title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
-      </View>
-      <View>
-        <View style={styles.box1}>
-          <Text>1</Text>
-        </View>
-        <View style={styles.box2}>
-          <Text>2</Text>
-        </View>
-        <View style={styles.box3}>
-          <Text>3</Text>
-        </View>
       </View>
     </View>
   );
@@ -28,35 +26,26 @@ export default function App() {
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
   inputContainer: {
     flexDirection: "row",
+    flex: 1,
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
   },
-  box1: {
-    width: 100,
-    height: 100,
-    backgroundColor: "red",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  box2: {
-    width: 100,
-    height: 100,
-    backgroundColor: "blue",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  box3: {
-    width: 100,
-    height: 100,
-    backgroundColor: "green",
-    justifyContent: "center",
-    alignItems: "center",
+  goalsContainer: {
+    flex: 4,
   },
 });
