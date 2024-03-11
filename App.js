59529d11b1e6421093245a53702180a5af275d36
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2024-03-11 13:36:45 +0000

    add background color

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import { StatusBar } from "expo-status-bar";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#ddb52f",
   },
 });
