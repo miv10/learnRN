0d046c19d5e40e25a1fbb23c513a76c7408c6dde
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2024-03-11 15:37:19 +0000

    skeleton of GameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,24 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 function GameScreen() {
-  return <Text>Game Screen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Oponent's guess</Text>
+      {/* Guess */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      <View>{/* Log Rounds */}</View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
 
 export default GameScreen;
